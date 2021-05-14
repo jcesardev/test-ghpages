f560f8397d4ef462917335d6fefde989a068b1f6
Julio Cesar Bolaños Palacios <julio@vetta.io>
2021-05-13 23:32:21 -0500

    Se carga otra version

---
+++ js/appart-custom.js
@@ -0,0 +1,18 @@
+;(function($){
+    "use strict";
+
+    $(document).ready(function () {
+
+        /*--------- Accordion js-----------*/
+        function fAqactive(){
+            $(".faq_accordian_two .card").on('click', function(){
+                $(".faq_accordian_two .card").removeClass("active");
+                $(this).addClass('active');
+            });
+        }
+        fAqactive();
+
+    }); // End Document.ready
+
+})(jQuery);
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
---
+++ js/sticky.js
@@ -0,0 +1,14 @@
+(function ($) {
+    "use strict";
+    // header sticky add class js
+    $(window).on('scroll', function () {
+        var window_top = $(window).scrollTop() + 0;
+        if (window_top > 0) {
+            $('.drdt_sticky_section').addClass('drdt_sticky_fixed');
+        } else {
+            $('.drdt_sticky_section').removeClass('drdt_sticky_fixed');
+        }
+    });
+
+}(jQuery));
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
